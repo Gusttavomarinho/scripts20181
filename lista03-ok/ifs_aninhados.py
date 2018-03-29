a83df18874198ff63f21535dc93b06d31b51376f
Gustavo Marinho <gusttavomarinho@gmail.com>
2018-03-29 13:28:28 -0300

    add aninhados

---
+++ lista03-ok/ifs_aninhados.py
@@ -0,0 +1,12 @@
+# Primeiro IF
+#
+if ​usuario == "Administrador"​:
+ # Segundo IF, aninhado dentro do primeiro pela indentação
+    if ​grupo == "Administradores"​:
+    print("Usuário autorizado."​)
+    # Else do segundo IF, na mesma indentação
+    else​:
+    print("Usuário não pertence ao gurpo ​\'​Administradores​\'​"​)
+    # Else do primeiro IF na mesma indentação
+else​:
+print("Usuário não é um administrador"​)
